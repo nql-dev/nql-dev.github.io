54249d832d0f9634e806808d8b941d4dbc84f639
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2023-07-27 07:48:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-echo(file_get_contents("https://x2-coin.000webhostapp.com/index.php"));
-?>
+<html>
+
+<body>
+  <h1>YOUR NAME</h1>
+  <p>Welcome to my website!</p>
+  <img src="https://content.codecademy.com/articles/github-pages-via-web-app/happy-ice-cream.gif" />
+</body>
+
+</html>
